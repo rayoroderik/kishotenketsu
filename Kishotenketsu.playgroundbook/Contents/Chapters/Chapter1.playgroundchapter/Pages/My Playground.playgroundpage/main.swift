a784f41568928a6bcb4cac8eb8a2035cb20a621a
Rayo <rayoroderik@gmail.com>
2020-05-14 11:35:57 +0700

    Initial playgroundbook commit

---
+++ Kishotenketsu.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
@@ -0,0 +1,9 @@
+
+//: ## Hello!
+//:
+//: My name is Rayo and this is Kishōtenketsu.
+//:
+//: I'll tell you about 4 pages comic strip and we'll make one together!
+//:
+//: [Click here to begin!](@next)
+//:
